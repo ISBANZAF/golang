3ad436654a039a2572a45c33598722c6e673aa18
ISBANZAF <isbanzaf22@gmail.com>
2025-12-23 18:45:22 +0100

    done

--- guess.go
+++ guess.go
@@ -9,13 +9,15 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	secret := rand.Intn(10) + 1
+	secret := rand.Intn(100) + 1
 
 	var guess int
-	fmt.Print("Guess the number (1–10): ")
+	fmt.Print("Guess the number (1 to 100): ")
 	fmt.Scanln(&guess)
 
-	for {
+	chance := 5
+
+	for chance > 0 {
 		if guess != secret {
 
 			if guess < secret {
@@ -26,9 +28,42 @@ func main() {
 			fmt.Println("try again: ")
 			fmt.Scanln(&guess)
 		}
+
+		chance --
+		fmt.Printf("you have %d chance left!\n", chance)
+
+
 		if guess == secret {
-			fmt.Println("Congratulatons The Number is:", guess)
-				break
+			fmt.Println("Congratulatons The Number is:", guess, "\n")
+
+			fmt.Println("moving to the next level!\n this will now add 50 to the former maximum number")
+			time.Sleep(time.Second * 2)
+
+			secret += 50 
+			chance += 2
+
+			fmt.Println("guess the number: ")
+			fmt.Scanln(&guess)
+
+				if guess != secret {
+
+			if guess < secret {
+		fmt.Println("the answer is greater than that!")
+	} else if guess > secret {
+		fmt.Println("the answer is less than that!")
+	}
+			fmt.Println("try again: ")
+			fmt.Scanln(&guess)
+		}
+
+		chance --
+		fmt.Printf("you have %d chance left!\n", chance)
+
+
+		if guess == secret {
+			fmt.Println("Congratulatons The Number is:", guess, "\n")
+			}
+
 		}
 	}
 }
